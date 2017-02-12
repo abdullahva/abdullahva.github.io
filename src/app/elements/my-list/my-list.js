684f6accc866ab7b0e8487a46c8cf5405798c993
abdullahva <abdullahva@gmail.com>
2017-02-12 13:07:27 -0500

    added firebase intg

--- src/app/elements/my-list/my-list.js
+++ src/app/elements/my-list/my-list.js
@@ -14,21 +14,18 @@ class MyList {
   beforeRegister() {
     this.is = 'my-list';
     this.properties = {
-      items: {
-        type: Array,
-        notify: true
+        companies: {
+          type: Object,
+          observer: 'dataChanged'
+        }
+      };
+
+      this.dataChanged = function(newData, oldData) {
+        console.log("newData " + JSON.stringify(newData));
       }
-    };
   }
   ready() {
-    this.items = [
-      'Responsive Web App boilerplate',
-      'Iron Elements and Paper Elements',
-      'End-to-end Build Tooling (including Vulcanize)',
-      'Unit testing with Web Component Tester',
-      'Routing with Page.js',
-      'Offline support with the Platinum Service Worker Elements'
-    ];
+    this.companies = [{companyName:"ralph"}];
   }
 }
 
