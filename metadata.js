44cc812967bd6caaa48e4cf7a27fba8403126374
abdullahva <abdullahva@gmail.com>
2017-02-13 22:02:45 -0500

    copied changes from dist

--- metadata.js
+++ metadata.js
@@ -1,16 +1,16 @@
 var config = require('../config');
 
 module.exports = {
-  title: 'Polymer Starter Kit +',
+  title: 'Resume E Michael Abdullah',
   description: 'Polymer Starter Kit Plus',
   charset: 'UTF-8',
   lang: 'en',
   // Add to homescreen for Chrome on Android
-  applicationName: 'PSK+',
+  applicationName: 'Resume',
   // Add to homescreen for Safari on iOS
-  appleMobileWebAppTitle: 'PSK+',
+  appleMobileWebAppTitle: 'Resume',
   // Google Analytics Tracking ID
-  googleAnalyticsTrackingId: 'UA-XXXXX-Y',
+  googleAnalyticsTrackingId: 'UA-91947001-1',
   // App name
   appName: config.appName,
   // App theme
